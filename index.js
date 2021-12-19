ca5598d3627ab619f66130451af168acc8e3bfe1
Sanic <sansunder718jqda@gmail.com>
2021-12-19 18:00:34 +0100

    Downgraded to DJS v12, fixed music stuff ig

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 //#region Essentials and Imports
 //#region Discord Essentials
+//Essentials
 const Discord = require('discord.js');
 const intents = new Discord.Intents(32767);
 const client = new Discord.Client({ intents });
 
 const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
-const Voice = require('@discordjs/voice');
+
+//Files and config
 const { token } = require('./Config/DiscToken.json');
 const DSettings = require('./Config/DSettings.json');
-const DiscCommands = require('./Helper/DiscCommands');
+const DCData = require('./Data/DiscCommands.json'); //Discord Command Data
+
 //#endregion
 //#region Import Modules
 const fs = require('fs');
@@ -24,6 +27,7 @@ const Load = require('./Helper/Load');
 const {ChannelIDSData, ChannelIDSDataLIST} = require('./Helper/Lists');
 //#endregion
 const RLCommands = require('./Helper/RLCommands');
+const yts = require('yt-search');
 //#endregion
 //#region Readline setup
 const rl = readline.createInterface({
@@ -36,9 +40,11 @@ const rl = readline.createInterface({
 
 client.on('ready', () => {
     client.user.setPresence({
-        activities: [{
-            name: Versions.discbver
-        }], status: DSettings.dstatus
+        status: DSettings.dstatus,
+        activity: {
+            name: Versions.discbver,
+            type: 'PLAYING',
+        },
     });
 
     Load('Channel IDS');
@@ -46,22 +52,62 @@ client.on('ready', () => {
     Log(`Logged in as ${client.user.tag}`, 0);
 
     RLCommands(rl, client);
-
-    DiscCommands(client, rl);
 });
 
-/*
-client.on('messageCreate', (message) => {
+client.on('message', async (message) => {
     if (!message.content.startsWith(DSettings.prefix) || message.author.bot) return;
     let args = message.content.substring(DSettings.prefix.length).split(" ");
 
-    if(args[0] === "ping")
+    if(args[0] === DCData.Ping.name)
     {
+        const calcping = new Discord.MessageEmbed()
+        .setTitle('Calculando el ping del bot...');
 
+        message.channel.send(calcping).then(resultMessage => {
+            const msgpingsomething = resultMessage.createdTimestamp - message.createdTimestamp;
+
+            const pingresult = new Discord.MessageEmbed()
+            .setTitle('Pong! :ping_pong:')
+            .addFields
+            (
+                { name: 'Latencia del bot ', value: `${msgpingsomething}ms`, inline: true},
+                { name: 'Ping del bot ', value: `${client.ws.ping}ms`, inline: true}
+            ).setColor('#008000');
+
+            resultMessage.edit(pingresult);
+        });
     }
-    else if(args[0] === "play")
+    else if(args[0] === DCData.Music.cmd1)
     {
+        //Now it doesn't crash, instead it only stops playing music lol, looking into it
+        const vc = message.member.voice;
 
+        if(!vc) return message.reply('Necesitas estar en un canal de voz');
+        if(!args[1]) return message.reply('Necesitas proporcionar una URL o una palabra para buscar');
+
+        const videoFinder = async (query) => {
+            const videoResult = await ytSearch(query);
+            return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
+        }
+
+        const video = await videoFinder(args.join(' '));
+
+        try {
+            let connection = await vc.channel.join();
+            const streamurl = ytdl(video.url, {filter: 'audioonly'});
+            connection.play(streamurl);
+
+            const MusicEmbed = new Discord.MessageEmbed()
+            .setTitle(`Reproduciendo ${video.title}`)
+            .setDescription(`${video.title}`)
+            .setImage(`${video.thumbnail}`)
+
+            message.channel.send(MusicEmbed);
+        }
+        catch (error) {
+            Log(error, 2);
+            return message.channel.send("Hubo un error intentando reproducir la cancion, error: " + error);
+        }
     }
     else if(args[0] === "stop")
     {
@@ -91,6 +137,5 @@ client.on('messageCreate', (message) => {
     {
 
     }
-})*/
-
+});
 client.login(token);
