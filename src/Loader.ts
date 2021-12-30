9e0d1c3d3e5bec2c4a4ce927c9fbbb9745c53c45
Sanic <sansunder718jqda@gmail.com>
2021-12-30 20:04:17 +0100

    Fixing loader and finishing functions

--- src/Loader.ts
+++ src/Loader.ts
@@ -1,8 +1,10 @@
+//#region Imports
 import LoaderThingy from './LoaderHelper/LoaderList';
 import { Logger } from './Logger';
 import TerminalConfig from './Config/TerminalConfig.json'
-import { SettingsHelper } from './LoaderHelper/SettingsHelper';
+//#endregion
 
+//clean the comments lol
 export async function InitLoad(){
     //this just calls every load function
     await LoadSettings().then(() => {
@@ -13,10 +15,25 @@ export async function InitLoad(){
 async function LoadSettings(){
     //now its the time where it comes the stupid logic lol
     //note after doing the whole for thingy, i thought i was able to access the json option number field thingy but i cant lol 
-    Logger(TerminalConfig.AmountOfOptions, 3);
+    //managed to do it, check the comment above the let TermConf xd
+    Logger(TerminalConfig.AmountOfOptions, 3); //it has the control of everything
     for(let funny = 1; funny < TerminalConfig.AmountOfOptions + 1; funny++)
     {
-        SettingsHelper(funny);
+        /* failed attempts or something, will remove after push
+        let what:Array<any> = [];
+        what.push(funny.toString());
+        Logger("Array stuff: " + what, 3);
+        what.forEach(function(value) {
+            TerminalConfig.Options[value];
+        })
+        SettingsHelper(funny);*/
+
+        //I dont know how it worked, I spent like 3 hours on trying to index the json file
+        let TermConf:any = TerminalConfig.Options;
+        //the ! is to tell typescript that im sure that it wont return null
+        Logger(TermConf[funny].option!, 3); 
+        Logger(TermConf[funny].state!, 3); 
+        Logger(TermConf[funny].value!, 3); 
     }
     return LoaderThingy[0].SettingsLoaded = true;
 }
