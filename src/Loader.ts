015a9b74990d72592445334f0b067642a6aa5b82
Sanic <sansunder718jqda@gmail.com>
2021-12-30 21:15:49 +0100

    Added a loading indicator or something

--- src/Loader.ts
+++ src/Loader.ts
@@ -14,7 +14,7 @@ export async function InitLoad(){
 }
 
 async function LoadSettings(){
-    Logger(TermConfigJSON.AmountOfOptions, 3); //it has the control of everything
+    //Logger(TermConfigJSON.AmountOfOptions, 3); //it has the control of everything
     for(let funny = 1; funny < TermConfigJSON.AmountOfOptions + 1; funny++)
     {
         /* failed attempts or something, will remove after push
@@ -26,19 +26,14 @@ async function LoadSettings(){
         })
         SettingsHelper(funny);*/
 
-        //the logger thingy will get deleted after this push
+        const LoadIndicator = `${funny}/${TermConfigJSON.AmountOfOptions}`; //amount of settings loaded and left to load ig
+
         let TermConf:any = TermConfigJSON.Options;
         //the ! is to tell typescript that im sure that it wont return null
-        Logger("JSON Option " + TermConf[funny].option!, 3);
-        Logger("JSON State " + TermConf[funny].state!, 3);
-        Logger("JSON Value " + TermConf[funny].value!, 3);
-        Logger("Loading settings", 0);
+        Logger("Loading settings " + LoadIndicator, 0);
         TermConfigList[funny].option! = TermConf[funny].option!;
         TermConfigList[funny].state! = TermConf[funny].state!;
         TermConfigList[funny].value! = TermConf[funny].value!;
-        Logger("List Option " + TermConfigList[funny].option!, 3);
-        Logger("List State " + TermConfigList[funny].state!, 3);
-        Logger("List Value " + TermConfigList[funny].value!, 3);
     }
     return LoaderThingy[0].SettingsLoaded = true;
 }
